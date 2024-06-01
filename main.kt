9c1e43ea46314367e60d74ec772ea99a4991d308
Mwakanemela <34372178-Mwakanemela@users.noreply.replit.com>
2024-06-01 07:22:07 +0000

    primary and secondary constructor

--- main.kt
+++ main.kt
@@ -1,3 +1,9 @@
 fun main() {
-  println("Hello world!")
+   val house = House(1, 5.5, "blue")
+}
+
+class House(color: String) {
+    constructor(rooms: Int, bathrooms: Double, color: String): this(color){
+        println("House color $color,  no. of $bathrooms, no. of rooms $rooms")
+    }
 }
