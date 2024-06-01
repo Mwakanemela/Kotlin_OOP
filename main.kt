c6fcc865e3fc29d522b7dd926cdc410c60586943
Mwakanemela <102220067+Mwakanemela@users.noreply.github.com>
2024-06-01 09:19:16 +0000

    Multiple init blocks

--- main.kt
+++ main.kt
@@ -1,16 +1,22 @@
 fun main() {
    val house = House(1, 5.5, "blue")
-   House("colorMwaka")
+   House("colorMwaka", 4)
 }
 
-class House(color: String) {
+class House(color: String, rooms: Int) {
 
     var color: String
+    var rooms: Int
     init {
         this.color = color
         println(this.color)
     }
-    constructor(rooms: Int, bathrooms: Double, color: String): this(color){
+
+    init {
+        this.rooms = rooms
+        println(this.rooms)
+    }
+    constructor(rooms: Int, bathrooms: Double, color: String): this(color, rooms){
         println("House color $color,  no. of $bathrooms, no. of rooms $rooms")
     }
 }
