1e7101d7ac44e3632f3ba6f37a407ae787f6416f
Mwakanemela <102220067+Mwakanemela@users.noreply.github.com>
2024-06-01 10:39:15 +0000

    methods - not all functions are methods(functions specific to class)

--- main.kt
+++ main.kt
@@ -1,6 +1,9 @@
 fun main() {
-   House(1, 5.5, "blue",500.5)
+   // House(1, 5.5, "blue",500.5)
    // House("colorMwaka", 4)
+    val house = House("blue", 5)
+    house.mediumFunction(true)
+    house.simpleFunction()
 }
 
 class House(color: String, rooms: Int) {
@@ -9,18 +12,26 @@ class House(color: String, rooms: Int) {
     var rooms: Int
     init {
         this.color = color
-        println(this.color)
+        // println(this.color)
     }
 
     init {
         this.rooms = rooms
-        println(this.rooms)
-    }
-    constructor(rooms: Int, bathrooms: Double, color: String): this(color, rooms){
-        println("House color $color,  no. of $bathrooms, no. of rooms $rooms")
+        // println(this.rooms)
     }
 
-    constructor(rooms: Int, bathrooms: Double, color: String, rentFee: Double):                 this(color, rooms){
-        println("rent fee $rentFee")
+    fun simpleFunction() {
+        println("This is a simple function")
     }
+
+    fun mediumFunction(mediumFunction: Boolean) {
+        println("This is is a medium function $mediumFunction")
+    }
+    // constructor(rooms: Int, bathrooms: Double, color: String): this(color, rooms){
+    //     println("House color $color,  no. of $bathrooms, no. of rooms $rooms")
+    // }
+
+    // constructor(rooms: Int, bathrooms: Double, color: String, rentFee: Double):                 this(color, rooms){
+    //     println("rent fee $rentFee")
+    // }
 }
