c2bcf5378b5ce5cf6fc16697df1742156e1ad291
Mwakanemela <102220067+Mwakanemela@users.noreply.github.com>
2024-06-01 09:14:07 +0000

    init block

--- main.kt
+++ main.kt
@@ -1,8 +1,15 @@
 fun main() {
    val house = House(1, 5.5, "blue")
+   House("colorMwaka")
 }
 
 class House(color: String) {
+
+    var color: String
+    init {
+        this.color = color
+        println(this.color)
+    }
     constructor(rooms: Int, bathrooms: Double, color: String): this(color){
         println("House color $color,  no. of $bathrooms, no. of rooms $rooms")
     }
